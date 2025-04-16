de6480cb05bdfc623ac403c13be871222ca16fd0
kwshal <kushal3738@gmail.com>
2025-04-16 10:14:26 +0530

    use abs URLs

--- sw.js
+++ sw.js
@@ -1,30 +1,31 @@
 const CACHE_NAME = "Copyer-cache-v1";
 
 self.addEventListener("install", (e) => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll([
-        "/Copyer/",
-        "/Copyer/index.html",
-        "/Copyer/manifest.json",
-        "/Copyer/sw.js",
-        "/Copyer/icon-256.png"      ]);
-    })
-  );
+     e.waitUntil(
+          caches.open(CACHE_NAME).then((cache) => {
+               return cache.addAll([
+                    "https://username.github.io/Copyer/",
+                    "https://username.github.io/Copyer/index.html",
+                    "https://username.github.io/Copyer/manifest.json",
+                    "https://username.github.io/Copyer/sw.js",
+                    "https://username.github.io/Copyer/icon-256.png"
+               ]);
+          })
+     );
 });
 
 self.addEventListener("activate", (e) => {
-  e.waitUntil(
-    caches.keys().then((keys) =>
-      Promise.all(
-        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
-      )
-    )
-  );
+     e.waitUntil(
+          caches.keys().then((keys) =>
+               Promise.all(
+                    keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+               )
+          )
+     );
 });
 
 self.addEventListener("fetch", (e) => {
-  e.respondWith(
-    caches.match(e.request).then((res) => res || fetch(e.request))
-  );
+     e.respondWith(
+          caches.match(e.request).then((res) => res || fetch(e.request))
+     );
 });
