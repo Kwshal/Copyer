fdfc5a6976a9f51616136d14129a07598a0c92a8
kwshal <kushal3738@gmail.com>
2025-04-16 09:12:45 +0530

    add caching

--- sw.js
+++ sw.js
@@ -1,23 +1,32 @@
-const CACHE_NAME = "clipsync-cache-v1";
-const FILES_TO_CACHE = [
-     "/",
-     "/index.html",
-     "/manifest.json",
-     "/script.js"
-];
+const CACHE_NAME = "Copyer-cache-v1";
 
-self.addEventListener("install", (event) => {
-     event.waitUntil(
-          caches.open(CACHE_NAME).then((cache) => {
-               return cache.addAll(FILES_TO_CACHE);
-          })
-     );
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll([
+        "/Copyer/",
+        "/Copyer/index.html",
+        "/Copyer/manifest.json",
+        "/Copyer/sw.js",
+        "/Copyer/icon192.png",
+        "/Copyer/icon512.png"
+      ]);
+    })
+  );
 });
 
-self.addEventListener("fetch", (event) => {
-     event.respondWith(
-          caches.match(event.request).then((response) => {
-               return response || fetch(event.request);
-          })
-     );
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+      )
+    )
+  );
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => res || fetch(e.request))
+  );
 });
