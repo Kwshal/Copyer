c5456097ff7e334afebd9867cc9c0dbfe33fa7d5
kwshal <kushal3738@gmail.com>
2025-04-16 10:22:49 +0530

    change url again

--- sw.js
+++ sw.js
@@ -4,16 +4,16 @@ self.addEventListener("install", (e) => {
      e.waitUntil(
           caches.open(CACHE_NAME).then((cache) => {
                return cache.addAll([
-                    "https://https://www.github.com/Kwshal/Copyer/",
-                    "https://https://www.github.com/Kwshal/Copyer/index.html",
-                    "https://https://www.github.com/Kwshal/Copyer/manifest.json",
-                    "https://https://www.github.com/Kwshal/Copyer/sw.js",
-                    "https://https://www.github.com/Kwshal/Copyer/icon-256.png"
+                    "https://www.github.com/Kwshal/Copyer/",
+                    "https://www.github.com/Kwshal/Copyer/index.html",
+                    "https://www.github.com/Kwshal/Copyer/manifest.json",
+                    "https://www.github.com/Kwshal/Copyer/sw.js",
+                    "https://www.github.com/Kwshal/Copyer/icon-256.png"
                ]);
           })
      );
 });
-console.log("url changed");
+console.log("url changed a");
 
 self.addEventListener("activate", (e) => {
      e.waitUntil(
